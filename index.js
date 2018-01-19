542457d3b77e9da5b9eba70c82898276c1cd1217
touir1 <touir.mat@gmail.com>
2018-01-19 01:32:15 +0100

    optimised nodejs server

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const port = 3000;
 
-const requestHandler = (req, res) => {
+const getFilePath = function(requestURL){
+    var filePath = './app' + requestURL;
+    if (filePath == './app/')
+        filePath = './app/index.html';
+    else if(filePath.indexOf('./app/node_modules')==0){
+        filePath = './'+filePath.substr(filePath.indexOf('node_modules'));
+    }
 
-    console.log('request: '+req.url);
+    return filePath;
+}
 
-    fs.readFile('./app/index.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
+const getContentType = function(extname){
+    var extensions = ['.js','.css','.json','.png','.jpg','.ico'];
+    var contentTypes = ['text/javascript','text/css','application/json','image/png','image/jpg','image/x-icon'];
+    var idx = extensions.indexOf(extname);
+    
+    return ((idx == -1)? 'text/html': contentTypes[idx]);
+}
+
+const requestHandler = (request, response) => {
+
+    console.log('request: '+request.url);
+
+    var filePath = getFilePath(request.url);
+    var extname = path.extname(filePath);
+    var contentType = getContentType(extname);
+
+    console.log('filePath: '+filePath);
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./app/404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
     });
 }
 
