ab705d18752596f653226792f46e286b7c738f29
touir1 <touir.mat@gmail.com>
2018-01-23 19:38:39 +0100

    Injected project in iframe

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,12 @@ $.get("/projectList", function(responseData) {
   var dirs = responseData.data;
   //console.log("Projects list: " + dirs);
   for (var i = 0; i < dirs.length; i++) {
-    console.log(dirs[i]);
+    var o = new Option(dirs[i].name , dirs[i].path + '/' + dirs[i].main);
+    $('#project-list').append(o);
   }
+});
+
+$('#open-button').on('click',function(ev){
+  var val = $('#project-list').val();
+  document.getElementById('loaded-app').src = val;
 });
